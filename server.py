403bb187f32921cf2332f43e8d0de24f6abf088a
rleonard67 <rleonard67@gmail.com>
2026-03-08 17:14:58 -0400

    Initial Commit

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Web app used to determine scores for different emotions"""
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +7,7 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    """The function analyzes entered text to determine scores for different emotions"""
     text_to_analyze = request.args.get('textToAnalyze')
     emotions = emotion_detector(text_to_analyze)
     anger = emotions['anger']
@@ -14,14 +17,16 @@ def emotion_analyzer():
     sadness = emotions['sadness']
     dominant_emotion = emotions['dominant_emotion']
     if dominant_emotion is None:
-        response = f"Invalid text! Please try again!"
-        return response
+        response = "Invalid text! Please try again!"
     else:
-        response = f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}. The dominant emotion is <strong>{dominant_emotion}</strong>."
-        return response
+        response = f"""For the given statement, the system response is 'anger': {anger},
+         'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}. The dominant
+          emotion is <strong>{dominant_emotion}</strong>."""
+    return response
 
 @app.route("/")
 def render_index_page():
+    """The function starts the web app"""
     return render_template('index.html')
 
 if __name__ == "__main__":
