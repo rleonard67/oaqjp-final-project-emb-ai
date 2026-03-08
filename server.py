f1790fe4277b7b064229d3e371ab052fa987ad09
rleonard67 <rleonard67@gmail.com>
2026-03-07 22:29:52 -0500

    Initial Commit

--- server.py
+++ server.py
@@ -6,20 +6,23 @@ app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def emotion_analyzer():
     text_to_analyze = request.args.get('textToAnalyze')
-    emotion_result = emotion_detector(text_to_analyze)
-    anger = emotion_result['anger']
-    disgust = emotion_result['disgust']
-    fear = emotion_result['fear']
-    joy = emotion_result['joy']
-    sadness = emotion_result['sadness']
-    dominant_emotion = emotion_result['dominant_emotion']
-    response = f"""For the given statement, the system response is
+    emotions = emotion_detector(text_to_analyze)
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+    dominant_emotion = emotions['dominant_emotion']
+    response_str = f"""For the given statement, the system response is
     'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}.
-    The dominant emotion is <strong>{dominant_emotion}</strong>
-    return response
+    The dominant emotion is <strong>{dominant_emotion}</strong>."""
+    return response_str
 
 @app.route("/")
 def render_index_page():
+    '''Render the index page to the user, this is where the text string to be
+    analyzed is provided and a response is displayed back to the user.
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
