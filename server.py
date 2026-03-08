115dac9bd6922d232789af12bf87296fa8c67153
rleonard67 <rleonard67@gmail.com>
2026-03-07 22:41:23 -0500

    Initial Commit

--- server.py
+++ server.py
@@ -13,16 +13,14 @@ def emotion_analyzer():
     joy = emotions['joy']
     sadness = emotions['sadness']
     dominant_emotion = emotions['dominant_emotion']
-    response_str = f"""For the given statement, the system response is
+    responses = f"""For the given statement, the system response is
     'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}.
     The dominant emotion is <strong>{dominant_emotion}</strong>."""
-    return response_str
+    #responses = "For the given statement, the system response is 'anger': " + anger 
+    return responses
 
 @app.route("/")
 def render_index_page():
-    '''Render the index page to the user, this is where the text string to be
-    analyzed is provided and a response is displayed back to the user.
-    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
